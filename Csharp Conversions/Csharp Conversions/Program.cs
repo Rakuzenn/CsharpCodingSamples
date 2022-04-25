00e783a58556f58a7fb7ba69ffea47c46c5c295d
Ricardo Silva <ricardo.silvaa2002@gmail.com>
2022-04-25 17:57:05 +0100

    More conversions

--- Csharp Conversions/Csharp Conversions/Program.cs
+++ Csharp Conversions/Csharp Conversions/Program.cs
@@ -40,8 +40,20 @@ namespace Csharp_Conversions
             bool sunIsShining = false; 
             string myBoolString = sunIsShining.ToString();
 
-            Console.WriteLine(myBoolString); 
+            Console.WriteLine(myBoolString);
 
+
+            // converter string to int
+
+            string myStringNum = "15";
+            string mySecondString = "13";
+            int num1 = Int32.Parse(myStringNum);
+            int num2 = Int32.Parse(mySecondString);
+            int resultInt = num1 + num2;
+            string result = myStringNum + mySecondString;
+
+            Console.WriteLine(resultInt);
+            Console.Read(); 
 
         }
     }
