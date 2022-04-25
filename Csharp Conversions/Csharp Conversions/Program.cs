dc2bff093f01a03339f473bfa531b9ad2848e8ba
Ricardo Silva <ricardo.silvaa2002@gmail.com>
2022-04-25 02:39:07 +0100

    Csharp Conversions file added

---
+++ Csharp Conversions/Csharp Conversions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Conversions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // implicit conversion
+
+
+            int num = 12390532;
+            long bigNum = num;
+
+            float myFloat = 13.37F;
+            double myNewDouble = myFloat;
+
+            double myDouble = 13.37;
+            int myInt;
+
+            //explicit conversion
+            // cast double to int;
+            // visto que e int nao detecta decimais o resultado de myInt sera 13 apenas.
+            myInt = (int)myDouble;
+            Console.WriteLine(myInt);
+
+            // typeConversion
+            // transforma o double em string e mostra o numero todo.
+            string myString = myDouble.ToString(); //"13.37"
+            string myFloatString =  myFloat.ToString();
+
+
+            Console.WriteLine(myFloatString);
+
+            // transforma o boolean em string escrevendo false na consola
+            bool sunIsShining = false; 
+            string myBoolString = sunIsShining.ToString();
+
+            Console.WriteLine(myBoolString); 
+
+
+        }
+    }
+}
